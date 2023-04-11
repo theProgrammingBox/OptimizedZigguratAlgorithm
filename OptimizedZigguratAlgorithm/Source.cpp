533beabd432b9bb4317dc186cf2796353d428e69
theProgrammingBox <justinboxgea@gmail.com>
2023-04-10 20:18:20 -0700

    inlined float rand and used uint8

--- OptimizedZigguratAlgorithm/Source.cpp
+++ OptimizedZigguratAlgorithm/Source.cpp
@@ -33,15 +33,6 @@ void r4_nor_setup(uint32_t kn[128], float fn[128], float wn[128])
     return;
 }
 
-//uint32_t shr3_seeded(uint32_t* jsr)
-//{
-//    uint32_t value = *jsr;
-//    *jsr = (*jsr ^ (*jsr << 13));
-//    *jsr = (*jsr ^ (*jsr >> 17));
-//    *jsr = (*jsr ^ (*jsr << 5));
-//    return value + *jsr;
-//}
-
 float r4_uni(uint32_t* jsr)
 {
     uint32_t jsr_input = *jsr;
@@ -59,7 +50,7 @@ TODO
 float r4_nor(uint32_t* jsr, const uint32_t kn[128], const float fn[128], const float wn[128])
 {
     int32_t randomInt32;
-    uint32_t sevenBits;
+    uint8_t sevenBits;
     const float r = 3.442620f;
     float x, y;
 
@@ -73,15 +64,27 @@ float r4_nor(uint32_t* jsr, const uint32_t kn[128], const float fn[128], const f
         return wn[sevenBits] * randomInt32;
     /*if ((randomInt32 < int32_t(kn[sevenBits]) && (~randomInt32 + 1) < int32_t(kn[sevenBits])) != (fabs(randomInt32) < kn[sevenBits]))
         printf("Error, %d, %f, %d, %d, %d, %d\n", randomInt32, fabs(randomInt32), kn[sevenBits], (~randomInt32 + 1), int32_t(randomInt32) < kn[sevenBits], (~randomInt32 + 1) < kn[sevenBits]);*/
-    
+
     for (;;)
     {
         if (sevenBits == 0)
         {
             for (;;)
             {
-                x = -0.2904764 * log(r4_uni(jsr));
-                y = -log(r4_uni(jsr));
+                x = *jsr;
+                *jsr = (*jsr ^ (*jsr << 13));
+                *jsr = (*jsr ^ (*jsr >> 17));
+                *jsr = (*jsr ^ (*jsr << 5));
+				x = (x + *jsr) * 2.3283064365386963e-10f;
+                x = -0.2904764 * log(x);
+
+				y = *jsr;
+				*jsr = (*jsr ^ (*jsr << 13));
+				*jsr = (*jsr ^ (*jsr >> 17));
+				*jsr = (*jsr ^ (*jsr << 5));
+				y = (y + *jsr) * 2.3283064365386963e-10f;
+				y = -log(y);
+                
                 if (x * x <= y + y)
                 {
                     if (randomInt32 <= 0)
@@ -91,8 +94,13 @@ float r4_nor(uint32_t* jsr, const uint32_t kn[128], const float fn[128], const f
             }
         }
 
+        y = *jsr;
+        *jsr = (*jsr ^ (*jsr << 13));
+        *jsr = (*jsr ^ (*jsr >> 17));
+        *jsr = (*jsr ^ (*jsr << 5));
+        y = (y + *jsr) * 2.3283064365386963e-10f;
         x = wn[sevenBits] * randomInt32;
-        if (fn[sevenBits] + r4_uni(jsr) * (fn[sevenBits - 1] - fn[sevenBits]) < exp(-0.5 * x * x))
+        if (fn[sevenBits] + y * (fn[sevenBits - 1] - fn[sevenBits]) < exp(-0.5 * x * x))
             return x;
 
         randomInt32 = *jsr;
@@ -103,31 +111,6 @@ float r4_nor(uint32_t* jsr, const uint32_t kn[128], const float fn[128], const f
         sevenBits = randomInt32 & 127;
         if (randomInt32 < int32_t(kn[sevenBits]) && (~randomInt32 + 1) < int32_t(kn[sevenBits]))
             return wn[sevenBits] * randomInt32;
-    }
-}
-
-void randomNormalSetup(uint32_t kn[128], float fn[128], float wn[128])
-{
-    double dn = 3.442619855899;
-    const double m1 = 2147483648.0;
-    const double vn = 9.91256303526217e-03;
-    double q = vn / exp(-0.5 * dn * dn);
-
-    kn[0] = dn / q * m1;
-    kn[1] = 0;
-    wn[0] = q / m1;
-    wn[127] = dn / m1;
-    fn[0] = 1.0;
-    fn[127] = exp(-0.5 * dn * dn);
-
-    double tn;
-    for (uint8_t i = 126; 1 <= i; i--)
-    {
-        tn = dn;
-        dn = sqrt(-2.0 * log(vn / dn + exp(-0.5 * dn * dn)));
-        kn[i + 1] = dn / tn * m1;
-        fn[i] = exp(-0.5 * dn * dn);
-        wn[i] = dn / m1;
     }
 }
 
@@ -188,22 +171,33 @@ float randomNormal(uint32_t& seed, const uint32_t kn[128], const float fn[128], 
 
 int main()
 {
-    uint32_t seed = std::chrono::duration_cast<std::chrono::nanoseconds>(std::chrono::high_resolution_clock::now().time_since_epoch()).count();
     uint32_t kn[128];
     float fn[128];
     float wn[128];
-
-    //randomNormalSetup(kn, fn, wn);
+    
     r4_nor_setup(kn, fn, wn);
 
-    const uint32_t bins = 42;
-    const uint32_t samples = 1000000;
+    const uint32_t bins = 54;
+    const uint32_t samples = 40000000;
     const float scale = 1000.0f / samples;
     const float min = -3.0f;
     const float max = 3.0f;
     const float bin_width = (max - min) / bins;
-    
-	auto start = std::chrono::high_resolution_clock::now();
+
+	float averageTime = 0.0f;
+    for (uint32_t j = 10; j--;)
+    {
+        uint32_t seed = std::chrono::duration_cast<std::chrono::nanoseconds>(std::chrono::high_resolution_clock::now().time_since_epoch()).count();
+        auto start = std::chrono::high_resolution_clock::now();
+        for (uint32_t i = samples; i--;)
+            r4_nor(&seed, kn, fn, wn);
+        auto stop = std::chrono::high_resolution_clock::now();
+        auto duration = std::chrono::duration_cast<std::chrono::nanoseconds>(stop - start);
+        averageTime += duration.count() * 1e-3f;
+    }
+	printf("Time taken: %f microseconds\n", averageTime * 0.1f);
+
+    uint32_t seed = std::chrono::duration_cast<std::chrono::nanoseconds>(std::chrono::high_resolution_clock::now().time_since_epoch()).count();
     uint32_t hist[bins];
     memset(hist, 0, sizeof(hist));
     for (uint32_t i = 0; i < samples; i++)
@@ -213,9 +207,6 @@ int main()
         if (bin < bins && bin >= 0)
             hist[bin]++;
     }
-	auto stop = std::chrono::high_resolution_clock::now();
-	auto duration = std::chrono::duration_cast<std::chrono::microseconds>(stop - start);
-	printf("Time taken: %lld microseconds\n", duration.count());
 
     printf("----------------------------------------\n");
     for (uint32_t i = 0; i < bins; i++)
