6ebae3a523a4102c75622449a1e7013bdcb76985
theProgrammingBox <justinboxgea@gmail.com>
2023-04-10 22:18:05 -0700

    I realized that I was not showing the entirty of the distribution

--- OptimizedZigguratAlgorithm/Source.cpp
+++ OptimizedZigguratAlgorithm/Source.cpp
@@ -4,26 +4,33 @@
 void r4_nor_setup(uint32_t kn[128], float fn[128], float wn[128])
 {
     double dn = 3.442619855899;
+    int i;
     const double m1 = 2147483648.0;
+    double q;
+    double tn = 3.442619855899;
     const double vn = 9.91256303526217E-03;
-    double q = vn / exp(-0.5 * dn * dn);
 
-    kn[0] = dn / q * m1;
+    q = vn / exp(-0.5 * dn * dn);
+
+    kn[0] = (uint32_t)((dn / q) * m1);
     kn[1] = 0;
-    wn[0] = q / m1;
-    wn[127] = dn / m1;
-    fn[0] = 1.0;
-    fn[127] = exp(-0.5 * dn * dn);
 
-    double tn;
-    for (uint8_t i = 126; 1 <= i; i--)
+    wn[0] = (float)(q / m1);
+    wn[127] = (float)(dn / m1);
+
+    fn[0] = 1.0;
+    fn[127] = (float)(exp(-0.5 * dn * dn));
+
+    for (i = 126; 1 <= i; i--)
     {
-        tn = dn;
         dn = sqrt(-2.0 * log(vn / dn + exp(-0.5 * dn * dn)));
-        kn[i + 1] = dn / tn * m1;
-        fn[i] = exp(-0.5 * dn * dn);
-        wn[i] = dn / m1;
+        kn[i + 1] = (uint32_t)((dn / tn) * m1);
+        tn = dn;
+        fn[i] = (float)(exp(-0.5 * dn * dn));
+        wn[i] = (float)(dn / m1);
     }
+
+    return;
 }
 
 uint32_t shr3_seeded(uint32_t* jsr)
@@ -60,8 +67,10 @@ float r4_uni(uint32_t* jsr)
 
 float r4_nor(uint32_t* jsr, uint32_t kn[128], float fn[128], float wn[128])
 {
-    int32_t hz;
+    int hz;
     uint32_t iz;
+    const float r = 3.442620;
+    float value;
     float x;
     float y;
 
@@ -69,7 +78,101 @@ float r4_nor(uint32_t* jsr, uint32_t kn[128], float fn[128], float wn[128])
     iz = (hz & 127);
 
     if (fabs(hz) < kn[iz])
-        return (float)(hz)*wn[iz];
+    {
+        value = (float)(hz)*wn[iz];
+    }
+    else
+    {
+        for (; ; )
+        {
+            if (iz == 0)
+            {
+                for (; ; )
+                {
+                    x = -0.2904764 * log(r4_uni(jsr));
+                    y = -log(r4_uni(jsr));
+                    if (x * x <= y + y)
+                    {
+                        break;
+                    }
+                }
+
+                if (hz <= 0)
+                {
+                    value = -r - x;
+                }
+                else
+                {
+                    value = +r + x;
+                }
+                break;
+            }
+
+            x = (float)(hz)*wn[iz];
+
+            if (fn[iz] + r4_uni(jsr) * (fn[iz - 1] - fn[iz])
+                < exp(-0.5 * x * x))
+            {
+                value = x;
+                break;
+            }
+
+            hz = (int)shr3_seeded(jsr);
+            iz = (hz & 127);
+
+            if (fabs(hz) < kn[iz])
+            {
+                value = (float)(hz)*wn[iz];
+                break;
+            }
+        }
+    }
+
+    return value;
+}
+
+void r4_nor2_setup(uint32_t kn[128], float fn[128], float wn[128])
+{
+    double dn = 3.442619855899;
+    int i;
+    const double m1 = 2147483648.0;
+    double q;
+    double tn = 3.442619855899;
+    const double vn = 9.91256303526217E-03;
+
+    q = vn / exp(-0.5 * dn * dn);
+
+    kn[0] = (uint32_t)((dn / q) * m1);
+    kn[1] = 0;
+
+    wn[0] = (float)(q / m1);
+    wn[127] = (float)(dn / m1);
+
+    fn[0] = 1.0;
+    fn[127] = (float)(exp(-0.5 * dn * dn));
+
+    for (i = 126; 1 <= i; i--)
+    {
+        dn = sqrt(-2.0 * log(vn / dn + exp(-0.5 * dn * dn)));
+        kn[i + 1] = (uint32_t)((dn / tn) * m1);
+        tn = dn;
+        fn[i] = (float)(exp(-0.5 * dn * dn));
+        wn[i] = (float)(dn / m1);
+    }
+
+    return;
+}
+
+float r4_nor2(uint32_t* jsr, uint32_t kn[128], float fn[128], float wn[128])
+{
+    int32_t hz;
+    uint8_t iz;
+    float x, y;
+
+    hz = shr3_seeded(jsr);
+    iz = hz & 127;
+    if (hz < int32_t(kn[iz]) && ~hz + 1 < int32_t(kn[iz]))
+        return wn[iz] * hz;
     
     for (;;)
     {
@@ -77,104 +180,27 @@ float r4_nor(uint32_t* jsr, uint32_t kn[128], float fn[128], float wn[128])
         {
             for (;;)
             {
-                x = -0.2904764 * log(r4_uni(jsr));
+                x = -0.2904764f * log(r4_uni(jsr));
                 y = -log(r4_uni(jsr));
                 if (x * x <= y + y)
                 {
-                    if (hz <= 0)
-                        return -3.442620f - x;
-                    return +3.442620f + x;
+                    x += 3.442620f;
+                    hz = hz & 0x80000000 ^ *(int32_t*)&x;
+                    return *(float*)&hz;
                 }
             }
         }
 
-        x = (float)(hz)*wn[iz];
-
-        if (fn[iz] + r4_uni(jsr) * (fn[iz - 1] - fn[iz]) < exp(-0.5 * x * x))
+        x = wn[iz] * int32_t(hz);
+        if (fn[iz] + r4_uni(jsr) * (fn[iz - 1] - fn[iz]) < exp(-0.5f * x * x))
             return x;
 
-        hz = (int)shr3_seeded(jsr);
-        iz = (hz & 127);
-
-        if (fabs(hz) < kn[iz])
-            return (float)(hz)*wn[iz];
+        hz = shr3_seeded(jsr);
+        iz = hz & 127;
+        if (hz < int32_t(kn[iz]) && ~hz + 1 < int32_t(kn[iz]))
+            return wn[iz] * hz;
     }
 }
-
-/*
-//TODO
-//0. see if kn can be int32_t
-//1. check wiki optimizations
-//2. unroll loop, get stats on how many iterations it takes
-
-float r4_nor(uint32_t* seed, const int32_t kn[128], const float fn[128], const float wn[128])
-{
-    int32_t randomInt32;
-    uint8_t sevenBits;
-    float x, y;
-
-    randomInt32 = *seed;
-    *seed = (*seed ^ (*seed << 13));
-    *seed = (*seed ^ (*seed >> 17));
-    *seed = (*seed ^ (*seed << 5));
-    randomInt32 += *seed;
-    sevenBits = randomInt32 & 127;
-    if (randomInt32 < kn[sevenBits] && ~randomInt32 + 1 < kn[sevenBits])
-        return wn[sevenBits] * randomInt32;
-    //if ((randomInt32 < int32_t(kn[sevenBits]) && (~randomInt32 + 1) < int32_t(kn[sevenBits])) != (fabs(randomInt32) < kn[sevenBits]))
-        //printf("Error, %d, %f, %d, %d, %d, %d\n", randomInt32, fabs(randomInt32), kn[sevenBits], (~randomInt32 + 1), int32_t(randomInt32) < kn[sevenBits], (~randomInt32 + 1) < kn[sevenBits]);
-
-    for (;;)
-    {
-        if (sevenBits == 0)
-        {
-            for (;;)
-            {
-				// randomInt32 is an int, so range is kinda messed up
-                randomInt32 = *seed;
-                *seed = (*seed ^ (*seed << 13));
-                *seed = (*seed ^ (*seed >> 17));
-                *seed = (*seed ^ (*seed << 5));
-				x = uint32_t(randomInt32 + *seed) * 2.3283064365386963e-10f;
-                //x = (*(int32_t*)&x - 0x3f800000) * -2.3935259956e-8f;
-                x = -0.2904764f * log(x);
-
-                randomInt32 = *seed;
-				*seed = (*seed ^ (*seed << 13));
-				*seed = (*seed ^ (*seed >> 17));
-				*seed = (*seed ^ (*seed << 5));
-				y = uint32_t(randomInt32 + *seed) * 2.3283064365386963e-10f;
-                //y = (*(int32_t*)&y - 0x3f800000) * -8.24e-8f;
-				y = -log(y);
-                
-                if (x * x <= y + y)
-                {
-                    x += 3.442620f;
-                    randomInt32 = *(uint32_t*)&x ^ randomInt32 & 0x80000000;
-                    return *(float*)&randomInt32;
-                }
-            }
-        }
-
-        randomInt32 = *seed;
-        *seed = (*seed ^ (*seed << 13));
-        *seed = (*seed ^ (*seed >> 17));
-        *seed = (*seed ^ (*seed << 5));
-        y = (randomInt32 + *seed) * 2.3283064365386963e-10f;
-        x = wn[sevenBits] * randomInt32;
-        if (fn[sevenBits] + y * (fn[sevenBits - 1] - fn[sevenBits]) < exp(-0.5f * x * x))
-            return x;
-
-        randomInt32 = *seed;
-        *seed = (*seed ^ (*seed << 13));
-        *seed = (*seed ^ (*seed >> 17));
-        *seed = (*seed ^ (*seed << 5));
-        randomInt32 += *seed;
-        sevenBits = randomInt32 & 127;
-        if (randomInt32 < kn[sevenBits] && ~randomInt32 + 1 < kn[sevenBits])
-            return wn[sevenBits] * randomInt32;
-    }
-}*/
 
 int main()
 {
@@ -182,17 +208,31 @@ int main()
     float fn[128];
     float wn[128];
     
+    uint32_t kn2[128];
+    float fn2[128];
+    float wn2[128];
+    
     r4_nor_setup(kn, fn, wn);
+	r4_nor2_setup(kn2, fn2, wn2);
 
-    const uint32_t bins = 54;
+    const uint32_t warmups = 20;
+    const uint32_t loops = 10;
+    const uint32_t bins = 128;
     const uint32_t samples = 40000000;
     const float scale = 1000.0f / samples;
-    const float min = -3.0f;
-    const float max = 3.0f;
+    const float min = -6.0f;
+    const float max = 6.0f;
     const float bin_width = (max - min) / bins;
+    
+    for (uint32_t j = warmups; j--;)
+    {
+        uint32_t seed = std::chrono::duration_cast<std::chrono::nanoseconds>(std::chrono::high_resolution_clock::now().time_since_epoch()).count();
+        for (uint32_t i = samples; i--;)
+            r4_nor(&seed, kn, fn, wn);
+    }
 
 	float averageTime = 0.0f;
-    for (uint32_t j = 10; j--;)
+    for (uint32_t j = loops; j--;)
     {
         uint32_t seed = std::chrono::duration_cast<std::chrono::nanoseconds>(std::chrono::high_resolution_clock::now().time_since_epoch()).count();
         auto start = std::chrono::high_resolution_clock::now();
@@ -202,30 +242,49 @@ int main()
         auto duration = std::chrono::duration_cast<std::chrono::nanoseconds>(stop - start);
         averageTime += duration.count() * 1e-3f;
     }
-	printf("Time taken: %f microseconds\n", averageTime * 0.1f);
+	printf("Time taken: %f microseconds\n", averageTime / loops);
 
+    averageTime = 0.0f;
+	for (uint32_t j = loops; j--;)
+	{
+		uint32_t seed = std::chrono::duration_cast<std::chrono::nanoseconds>(std::chrono::high_resolution_clock::now().time_since_epoch()).count();
+		auto start = std::chrono::high_resolution_clock::now();
+		for (uint32_t i = samples; i--;)
+			r4_nor2(&seed, kn2, fn2, wn2);
+		auto stop = std::chrono::high_resolution_clock::now();
+		auto duration = std::chrono::duration_cast<std::chrono::nanoseconds>(stop - start);
+		averageTime += duration.count() * 1e-3f;
+	}
+	printf("Time taken: %f microseconds\n", averageTime / loops);
+    
     uint32_t seed = std::chrono::duration_cast<std::chrono::nanoseconds>(std::chrono::high_resolution_clock::now().time_since_epoch()).count();
     uint32_t hist[bins];
     memset(hist, 0, sizeof(hist));
     for (uint32_t i = 0; i < samples; i++)
     {
-        //uint32_t bin = (randomNormal(seed, kn, fn, wn) - min) / bin_width;
         uint32_t bin = (r4_nor(&seed, kn, fn, wn) - min) / bin_width;
         if (bin < bins && bin >= 0)
             hist[bin]++;
     }
 
-    printf("----------------------------------------\n");
+	seed = std::chrono::duration_cast<std::chrono::nanoseconds>(std::chrono::high_resolution_clock::now().time_since_epoch()).count();
+	uint32_t hist2[bins];
+	memset(hist2, 0, sizeof(hist2));
+	for (uint32_t i = 0; i < samples; i++)
+	{
+		uint32_t bin = (r4_nor2(&seed, kn2, fn2, wn2) - min) / bin_width;
+		if (bin < bins && bin >= 0)
+			hist2[bin]++;
+	}
+    
     for (uint32_t i = 0; i < bins; i++)
     {
-        /*for (uint32_t j = scale * hist[i]; j--;)
-            printf("*");
-        printf("\n");*/
-        
-		std::string spaces(scale * hist[i], ' ');
-		printf("%s%f\n", spaces.c_str(), scale * hist[i]);
+        printf("%f\n", fabs(scale * hist[i] - scale * hist2[i]));
+        std::string spaces(scale * hist[i], ' ');
+        printf("\t%s*%f\n", spaces.c_str(), scale * hist[i]);
+		spaces = std::string(scale * hist2[i], ' ');
+		printf("\t%s*%f\n", spaces.c_str(), scale * hist2[i]);
     }
-    printf("----------------------------------------\n");
 
     return 0;
 }
